e26db9eb7bd4bf8031bc428b6eca0e78720a4bc9
KeyF <filasteixeira@hotmail.com>
2018-10-18 19:12:28 +0100

    Troca

---
+++ Escalonamento/Escalonamento/Models/Trocas.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Escalonamento.Models
+{
+    public class Trocas
+    {
+        public int IdTrocas { get; set; }
+
+        [Required]
+        public string Nome1 { get; set; }
+        [Required]
+        public string Turno1 { get; set; }
+        [Required]
+        public decimal Nome2 { get; set; }
+        [Required]
+        public string Turno2 { get; set; }
+    }
+}
