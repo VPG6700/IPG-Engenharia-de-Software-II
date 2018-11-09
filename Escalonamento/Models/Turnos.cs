54b71f0046c6a4d40f3a76c23d9041960265a42e
1012058 <1012058@sal.ipg.pt>
2018-11-09 12:06:55 +0000

    trurn

--- Escalonamento/Models/Turnos.cs
+++ Escalonamento/Models/Turnos.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,9 @@ namespace Escalonamento.Models
 {
     public class Turnos
     {
+        [Key]
+        public int IdTurnos { get; set; }
+
         public string TurnoManha { get; set; }
 
         public string TurnoTarde { get; set; }
